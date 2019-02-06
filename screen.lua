1c7fe6183f04fde6f70e55c65fb12d0474bb7922
Person124 <calum.phillips.code@gmail.com>
2019-02-06 01:21:59 +0000

    Added comments and now the subscreen now renders in the middle of the current screen

--- screen.lua
+++ screen.lua
@@ -1,27 +1,47 @@
 screen = {}
 
+-- This function generates the sub screen and does necessary math
+-- to set it up
 function screen.load()
+	-- This is the resolution of the subscreen
 	screen.baseWidth = 960
 	screen.baseHeight = 640
 
+	-- a canvas to draw the subscreen to
 	screen.canvas = love.graphics.newCanvas(screen.baseWidth, screen.baseHeight)
 
+	-- Get the screen width and height, necessary for math
 	local screenWidth = love.graphics.getWidth()
 	local screenHeight = love.graphics.getHeight()
 
+	-- attempt to make a scaler value based on height
 	local scaler = screenHeight / screen.baseHeight
 
+	-- Check to see if it works for width as well
 	if screen.baseWidth > screenWidth then
+		-- if not make scaler value on width
 		scaler = screenWidth / screen.baseWidth
 	end
 
+	-- Apply whichever scaler value was used
 	screen.width = screen.baseWidth * scaler
 	screen.height = screen.baseHeight * scaler
 	screen.scaler = scaler
 
+	-- Calculate the center point of the screen to figure out where to draw
+	-- the subscreen to center it
+	local centerX = screenWidth / 2
+	local centerY = screenHeight / 2
+
+	-- Calculate the draw point including offsets
+	screen.drawX = centerX - (screen.width / 2)
+	screen.drawY = centerY - (screen.height / 2)
+
+	-- set the blendmode, shouldn't need to be changed ever
 	love.graphics.setBlendMode("alpha")
 end
 
+-- This function enables drawing to the subscreen
 function screen.start()
 	love.graphics.setColor(255, 255, 255)
 	love.graphics.setLineWidth(10)
@@ -31,9 +51,14 @@ function screen.start()
 	love.graphics.rectangle('line', 0, 0, screen.baseWidth, screen.baseHeight)
 end
 
-function screen.stop()
+-- The function stops drawing to the subscreen as well as draws the subscreen
+-- centered. If a parameter is given then it WON'T draw the subscreen
+function screen.stop(draw)
 	love.graphics.setCanvas()
 
 	love.graphics.setColor(255, 255, 255)
-	love.graphics.draw(screen.canvas, 0, 0, 0, screen.scaler, screen.scaler)
+
+	if draw == nil then
+		love.graphics.draw(screen.canvas, screen.drawX, screen.drawY, 0, screen.scaler, screen.scaler)
+	end
 end
