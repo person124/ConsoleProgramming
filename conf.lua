52d72870a79993eff430101170e22184ef8cfdea
Person124 <calum.phillips.code@gmail.com>
2019-02-07 04:29:12 +0000

    Disabled unnecessary modules

--- conf.lua
+++ conf.lua
@@ -3,4 +3,13 @@ function love.conf(t)
 	t.console = false
 
 	t.window.title = "No name game! :D"
+
+	t.modules.joystick = false
+	t.modules.keyboard = false
+	t.modules.math = false
+	t.modules.physics = false
+	t.modules.system = false
+	t.modules.timer = false
+	t.modules.video = false
+	t.modules.thread = false
 end
