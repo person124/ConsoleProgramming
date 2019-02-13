14637d67a9b5523257decaa3dd2e177fb9397fa6
Person124 <calum.phillips.code@gmail.com>
2019-02-13 21:09:43 +0000

    Added position tracking to input

--- input.lua
+++ input.lua
@@ -6,29 +6,39 @@ input = {}
 
 function input.load()
 	input.count = 0 -- Keeps track of the number of touches
+	input.x = 0 -- x location of the touch
+	input.y = 0 -- y location of the touch
 
 	input.mouse = {}
 	for i=1,3 do
 		input.mouse[i] = false
 	end
-	input.mouse.x = 0
-	input.mouse.y = 0
 end
 
 -- Built in function that is called on touch start
 function love.touchpressed(id, x, y, dx, dy, pressure)
 	input.count = input.count + 1
+	input.x = x
+	input.y = y
 end
 
 
 -- Built in function that is called on touch release
 function love.touchreleased(id, x, y, dx, dy, pressure)
 	input.count = input.count - 1
+	
+	if input.count == 0 then
+		input.x = 0
+		input.y = 1
+	end
 end
 
 -- Built in function that is called when a current touch moves
 function love.touchmoved(id, x, y, dx, dy, pressure)
-	if input.count == 2 then 
+	if input.count == 1 then
+		input.x = x
+		input.y = y
+	elseif input.count == 2 then 
 		screen.addOffset(dx, dy)
 	end
 end
@@ -47,8 +57,8 @@ end
 
 -- Built in function that has the position and button of a mouse click
 function love.mousepressed(x, y, button)
-	input.mouse.x = x
-	input.mouse.y = y
+	input.x = x
+	input.y = y
 	input.mouse[button] = true
 end
 
@@ -56,14 +66,14 @@ end
 -- Built in function that has the position and button of a released mouse click
 function love.mousereleased(x, y, button)
 	input.mouse[button] = false
-	input.mouse.x = x
-	input.mouse.y = y
+	input.x = x
+	input.y = y
 end
 
 -- Built in function that has the position of a moved mouse click
 function love.mousemoved(x, y, dx, dy)
-	input.mouse.x = x
-	input.mouse.y = y
+	input.x = x
+	input.y = y
 
 	if input.mouse[2] then
 		screen.addOffset(dx, dy)
