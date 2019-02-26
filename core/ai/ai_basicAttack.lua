a4b4d5b26907ba6624b7f67627117753a3443aac
Calum Phillips <calum.phillips.codes@gmail.com>
2019-02-25 21:21:05 -0500

    Added the prototype and steps to handle basic attacking

---
+++ core/ai/ai_basicAttack.lua
@@ -0,0 +1,15 @@
+--[[
+	This function will take in the map, entity that is attacking, the target,
+	the movement map, and the attacking map. As a result it will attack the specified entity
+	while moving the attacker to a suitable space.
+--]]
+
+function basicAttack(map, ent, target, moveMap, attackMap)
+	-- Steps:
+	-- 1) Get range
+	-- 2) Get movement tile at specified range
+	-- 3) Move unit
+	-- 4) Apply damage
+end
+
+return basicAttack
