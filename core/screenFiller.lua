9884d936ea9f5ada9a75441ba03f5cabb91b01c4
Person124 <calum.phillips.code@gmail.com>
2019-02-28 20:23:02 +0000

    Got the screen filler mostly working

--- core/screenFiller.lua
+++ core/screenFiller.lua
@@ -1,16 +1,27 @@
-local screenFiller = love.graphics.newCanvas(love.graphics.getWidth(), love.graphics.getHeight());
+local width = love.graphics.getWidth()
+local height = love.graphics.getHeight()
+local lineCount = 10
+
+local screenFiller = love.graphics.newCanvas(width, height);
 love.graphics.setBlendMode("alpha")
 
 love.graphics.setCanvas(screenFiller)
-love.graphics.clear(255, 255, 255);
+love.graphics.clear(54, 35, 54);
 
-love.graphics.setColor(0, 255, 0, 255)
+love.graphics.setColor(196, 151, 48)
 love.graphics.setLineWidth(2)
-for i=1,100 do
-	local x = screenFiller:getWidth() / 100 * i
-	local y = screenFiller:getHeight() / 100 * i
-	print(x, y)
+for i=1,lineCount do
+	local x = width / lineCount * i
+	local y = height / lineCount * i
+
 	love.graphics.line(x, 0, 0, y)
+end
+
+for i=1,lineCount do
+	local x = width / lineCount * i
+	local y = height / lineCount * i
+
+	love.graphics.line(x, height, width, y)
 end
 
 love.graphics.setCanvas()
