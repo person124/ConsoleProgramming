e3b31ef87bbdf0b0b3b187e6c2c8a89d453c9fa5
Calum Phillips <calum.phillips.code@gmail.com>
2019-03-03 16:16:15 -0500

    Finished the basic loop!!!!!!!

--- core/ai/ai_enemyBasic.lua
+++ core/ai/ai_enemyBasic.lua
@@ -7,7 +7,6 @@ function enemyBasic(map, ent, playerEnts)
 	-- Get the tiles
 	local moveTiles, attackTiles = ai.plan(map, ent)
 
-	local distance = 10000
 	for i=1,table.getn(attackTiles) do
 		local enemy = map.getEntity(attackTiles[i].x, attackTiles[i].y)
 		if enemy ~= nil then
@@ -16,7 +15,24 @@ function enemyBasic(map, ent, playerEnts)
 		end
 	end
 	
-	-- TODO add something if no entities within range
+	-- If not within attacking range then find the next best thing
+	local distance = 10000
+	local path = nil
+	
+	-- Get the closest entity
+	local start = utils.getPoint(ent.x, ent.y)
+	for i=1,table.getn(playerEnts) do
+		local finish = utils.getPoint(playerEnts[i].x, playerEnts[i].y)
+		local currentPath = ai.aStar(map, start, finish)
+		
+		if table.getn(currentPath) < distance then
+			distance = table.getn(currentPath)
+			path = currentPath
+		end
+	end
+	
+	-- Move towards the closest entity
+	map.moveEntity(ent, path[ent.sp].x, path[ent.sp].y)
 end
 
 return enemyBasic
