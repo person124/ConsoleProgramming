e82f1c60667dfc06d41659dbf301f1b75c20c7a2
Calum Phillips <calum.phillips@mymail.champlain.edu>
2019-04-14 21:29:07 -0400

    Added quit button and option for level editor

--- core/modes/mainMenu.lua
+++ core/modes/mainMenu.lua
@@ -13,14 +13,21 @@ function mainMenu.update(dt)
 	if input.screenX ~= -1 and input.screenY ~= -1 and input.count == 1 then
 		local x = input.screenX
 		local y = input.screenY
+		--print(x, y)
 
 		if x >= 84 and x <= 400 and y >= 300 and y <= 364 then
 			goToLevelSelect()
+		elseif x >= 84 and x <= 485 and y >= 400 and y <= 445 then
+			print("Level Editor")
+		elseif x >= 84 and x <= 220 and y >= 490 and y <= 530 then
+			love.event.quit(0)
 		end
 	end
 end
 
 function mainMenu.render(screen)
+	love.graphics.setColor(255, 255, 255)
+
 	-- Render main title
 	love.graphics.setNewFont(100)
 	love.graphics.print("Tactical Knockout", 50, 50)
@@ -28,9 +35,6 @@ function mainMenu.render(screen)
 	love.graphics.setNewFont(64)
 
 	-- Render menu options
-	love.graphics.setColor(255, 0, 0)
-	love.graphics.rectangle("fill", 20+64, yMod+55, 32 * 10, 64)
-	love.graphics.setColor(255, 255, 255)
 	love.graphics.print("Play Level", 20 + 64, yMod + 55)
 
 	if not utils.isMobile() then
